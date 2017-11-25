bbcb7b9a42b28ae04a2a13ce4d71c0dff0bbc7e7
xuan <anxu5829@gmail.com>
2017-11-25 20:10:49 +0800

    solve some bugs , using loops

--- yPrepare.py
+++ yPrepare.py
@@ -1,11 +1,24 @@
 # prepare S
 
-def s_usrPrepare():
+def s_usrPrepare(u):
     # return Susr
+    # Susr = ( u * omiga)
     pass
 
 
-def s_itemPrepare():
+def s_itemPrepare( i ):
     # return Siem
+    # Si_omiga = ( i * omiga )
     pass
 
+def sPrepare(susr,sitem,k,mode = "u"):
+    # mode 取u or i , u 表示做user 回归，从usr中取一行
+    # k 表示做到第k行、列
+    if (mode == "u"):
+        return(sitem*susr[k,:])
+    elif(mode == "i"):
+        return(susr*sitem[k,:])
+
+
+def yPrepare(S_ui_omiga,X_ui_omiga):
+    pass
