ff90750d085379163de098fc7f63ee321432f3c3
anxu5829 <anxu5829@gmail.com>
2017-12-01 17:08:39 +0800

    add item_attr_matrix calculate method

--- yPrepare.py
+++ yPrepare.py
@@ -1,7 +1,16 @@
 # prepare User distance square
-import numpy as np
+import  numpy as np
+import  pandas as pd
+import  os
+from    scipy.sparse import csr_matrix
+from    scipy.sparse import csc_matrix
+from    sklearn.preprocessing import LabelEncoder
 import  sklearn.metrics.pairwise as smp
-
+import  gc
+from itertools import chain
+import dask.array as da
+import collections
+import re
 
 
 def calculateDis(ObjectAttr,attrNum=2,ObjectNum = 1e4):
@@ -29,7 +38,6 @@ def calculateDis(ObjectAttr,attrNum=2,ObjectNum = 1e4):
 
 def calculateNet(ObjectAttr,attrNum,ObjectNum):
     """
-
     :param ObjectAttr: must be a matrix has a shape like (ObjectNum *　attrNum)
     :param attrNum: the  number of categorical attr a usr have
     :param ObjectNum: the number of usr we observed
@@ -38,9 +46,10 @@ def calculateNet(ObjectAttr,attrNum,ObjectNum):
     ObjectAttr matrix is ObjectNum * attrNum matrix
     target is to return a Object *　Object matrix  ， saving value of
     the social net work of each Object
-
     """
     #　return a test matrix for other
+
+
     return np.ones((ObjectNum,ObjectNum))
 
 
@@ -110,18 +119,222 @@ def yGenerator(ObjectNum,mode = "usr",parallel = True):
         elif mode == "item":
             pass
 
+############# method for data prepare#####################
 
-# 可再进一步开发q的回归工具
+# fill na
+def fillNAN(tableName,values):
+    # values = {'genre_ids' : 'unkown' }
+    tableName = tableName.fillna(value = values)
+    return  tableName
 
 
+def changeNameToID(tableName,id , plan = 'A'):
+    """
+
+    :param tableName: the table name
+    :param id: the primary id
+    :param plan:plan A uses category.cat.codes to change the id , plan B use sklearn's encoder to encode id
+    :return: return a tuple ,of which first is the table using encoded id , second is the original id
+    """
+    if plan == 'A':
+
+        originalName = tableName[id]
+
+        originalName_index = originalName.index
+
+        originalNameUnique = originalName.unique()
+
+        originalNameCategory = originalName.astype("category", categories=originalNameUnique).cat.codes
+
+        tableName[id] = originalNameCategory
+
+        del originalName_index,originalNameUnique,originalNameCategory
+
+        return(tableName,originalName)
+
+    elif plan == 'B':
+
+        le = LabelEncoder()
+
+        le.fit(tableName[id])
+
+        originalName = tableName[id]
+
+        tableName[id] = le.transform(tableName[id])
+
+        return (tableName,originalName)
+
+
+def splitDF(tableName,id,colListContinue,colListDiscrete):
+    colListContinue.append(id)
+    colListDiscrete.append(id)
+    itemCntnueAttr  = tableName[colListContinue]
+    itemDscrtAttr   = tableName[colListDiscrete]
+    return (itemCntnueAttr,itemDscrtAttr)
+
+
+def tagCombine(tableName, tagColList, id, split="|"):
+    #temp  =   tableName.set_index(id)
+    for i in tagColList:
+        tableName.loc[:,i] = tableName[i].str.strip()
+
+    tag0 = tagColList[0]
+    temp = pd.DataFrame()
+    temp["concat"] = tableName[tag0].str.cat([ tableName[i] for i in tagColList if i != tag0],sep = split)
+    temp = temp.set_index(tableName[id])
+    return(temp)
+
+
+
+
+
+
+
+def findNetwork(tableName,fillnawith,split = r"&|\|" ):
+
+    #tableName = songtag;split = "|" ,
+
+    # select useful columns
+    temp = tableName.copy()
+    temp.reset_index(inplace =True)
+    idname = tableName.index.name
+    id = temp[idname]
+
+
+    # split colName based on var split , due to orignal data has sth like "a|b"
+    temp['splitTag'] = temp['concat'].str.split(split)
+
+    contains = r''+'.*' + '.*'.join(fillnawith.values()) + '.*' + ''
+
+    temp['hasNoTag'] = temp.concat.str.contains(contains)
+
+    temp.drop( ["concat"] ,axis = 1 , inplace=True)
+
+
+    temp['lenOfType'] = temp['splitTag'].map(lambda  x : len(x))
+
+    objectHasNoTag = (id[temp.hasNoTag == True])
+
+
+    colNameSpread = [ j  for i in temp['splitTag'] for j in i]
+    idSpread = [ [id[i]]*j for i,j in enumerate(temp['lenOfType'])]
+    idSpread = [j for i in idSpread for j in i]
+
+    # id_colNameDF has the rows which contain the id-tag pair
+    id_colNameDF = pd.DataFrame({idname:idSpread,'tag':colNameSpread})
+
+    del temp,idSpread,colNameSpread
+    gc.collect()
+
+
+    # use label encoder to transform genre_id so that we can build a spasre - matrix
+
+    # id_colNameDF[id_colNameDF[id].isin(objectHasNoTag)]
+
+    id_colNameDF = id_colNameDF[-id_colNameDF.tag.isin([ str(i)  for  i in fillnawith.values()])]
+
+
+    id_colNameDF ,colNameList = changeNameToID(id_colNameDF,'tag' ,plan="B" )
+
+
+    id_colNameDF[idname] = id_colNameDF[idname].astype(int)
+    id_colNameDF['tag'] = id_colNameDF['tag'].astype(int)
+
+    #flag = id_colNameDF.ix[colNameList.astype(int) == -1,'genre_ids'].values[0]
+
+
+    #id_colNameDF.ix[colNameList.astype(int) == -1,'target'] = -1
+    #id_colNameDF.ix[colNameList in fillnawith.values() , 'target'] = -1
+
+    maxcoder = max(id_colNameDF.tag.unique())
+    id_colNameDF2 = pd.DataFrame({idname:objectHasNoTag,'tag':maxcoder+1})
+
+    id_colNameDF['target'] = 1
+    id_colNameDF2['target'] = -1
+
+    id_colNameDF = pd.concat([id_colNameDF,id_colNameDF2])
+
+    del id_colNameDF2
+    gc.collect()
+    ObjectTagmatrix = csc_matrix((id_colNameDF['target'], (id_colNameDF[idname], id_colNameDF['tag'])))
+    del id_colNameDF
+    gc.collect()
+
+    return(ObjectTagmatrix)
+
+
+##########################################################
 
 
 if __name__=="__main__":
+
+
+    # change dir
+    os.chdir("/home/xuan/桌面/recommand Sys/")
+
+    # load item data
+    item = pd.read_csv("songsCSV.csv",encoding="UTF-8" ,dtype = {
+        "song_length": np.uint16,
+        "language" : str
+    })
+
+# use for debug
+    item.loc[2296320,'song_id'] = 'special'
+
+    # fill na use default value , this value is also used in build social network
+    fillnawith = collections.OrderedDict()
+    fillnawith['genre_ids'] = '-1'
+    fillnawith['artist_name'] = 'no_artist'
+    fillnawith['composer']= 'no_composer'
+    fillnawith['language'] = '-1'
+
+    item = fillNAN(item, fillnawith)
+
+    # fill na with special value calculated from data
+    pass
+
+
+    # change primary key to ID
+    item , song_id = changeNameToID(item, 'song_id' , plan = "B")
+
+
+    # split the dataframe to two , one of it is containing  the continue attr
+    # the other containing the discrete attr
+
+    ( itemCntnueAttr , itemDscrtAttr ) = \
+        splitDF(item,"song_id",
+                ["song_length"],
+                ["genre_ids","artist_name","composer","language"]
+                )
+    del item ; gc.collect;
+
+
+
+    # create socail network of item using dask
+
+    # do the tag combine process
+
+    id = "song_id"
+    colList = itemDscrtAttr.columns.tolist()
+    colList.remove(id)
+
+    itemWithTag = tagCombine(itemDscrtAttr, id='song_id', tagColList=colList)
+
+    itemTagmatrix = findNetwork(itemWithTag,  fillnawith , split = r"&|\|")
+
+
+    itemNet = calculateNet(1, 1, 22 * 1e4)
+
+    # usrNet = calculateNet(1,1,1*1e4)
+
+
+
     # generate two dis for test later method
     usrDis = calculateDis(1,2,1*1e4)
     itemDis = calculateDis(1,2,22*1e4)
-    usrNet = calculateNet(1,1,1*1e4)
-    itemNet = calculateNet(1,1,22*1e4)
+
+
+
     (ulist, ilist , rlist) = subtractIdx(1,180*1e4,encoded=True)
 
 
